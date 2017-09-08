16b3927dd1e9408c800ea66b37126c1e31858689
hyy369 <hyy369@users.noreply.github.com>
2017-09-07 21:39:42 -0400

    remake

--- Debug/CMakeFiles/helloworld.dir/DependInfo.cmake
+++ Debug/CMakeFiles/helloworld.dir/DependInfo.cmake
@@ -1,21 +1,22 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/anon/Workspace/pong/bin/helloworld.cpp" "/home/anon/Workspace/pong/Debug/CMakeFiles/helloworld.dir/bin/helloworld.cpp.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/HYY/Documents/Workspace/Pong/bin/helloworld.cpp" "/Users/HYY/Documents/Workspace/Pong/Debug/CMakeFiles/helloworld.dir/bin/helloworld.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
-  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
+  "/Library/Frameworks/SFML.framework"
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
